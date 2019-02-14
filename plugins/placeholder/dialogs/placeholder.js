6bd9be330f22157ed85f6f19e9cc1fef98bf39c6
devaptas <36535740+devaptas@users.noreply.github.com>
2019-02-14 12:36:38 -0200

    plugin para substituição de variáveis no editor

---
+++ plugins/placeholder/dialogs/placeholder.js
@@ -0,0 +1,60 @@
+'use strict';
+
+CKEDITOR.dialog.add( 'placeholder', function( editor ) {
+
+	return {
+		title: 'Substituir variável',
+		minWidth: 500,
+		minHeight: 100,
+		contents: [
+			{
+				id: 'var',
+				elements: [
+					{
+						id: 'name',
+						type: 'html',
+						html: '<div id="values-container"></div>',
+						style: 'width: 100%;',
+						required: true,
+						setup: function( widget ) {
+							// Ao carregar o dialog, limpa o html dos valores anteriores e
+							// remove as classes do CKEDITOR que interferem com o css.
+							$('#values-container').html('');
+							$('.cke_reset_all').removeClass('cke_reset_all');
+							$('.cke_dialog_ui_button').addClass('btn btn-xs').removeClass('cke_dialog_ui_button');
+							$('.cke_dialog_ui_button_ok').addClass('btn-success').removeClass('cke_dialog_ui_button_ok');
+							$('.cke_dialog_ui_button_cancel').addClass('btn-danger').removeClass('cke_dialog_ui_button_cancel');
+
+			                $.ajax({
+			                    url: editor.config.placeholderUrl,
+			                    data: {name: widget.data.name},
+			                    success: function(data) {
+									$('#values-container').append(data.htmlMessage);
+			                    }
+			                });
+						},
+						commit: function( widget ) {
+							// Quando o botão "OK" é pressionado, insere o valor da variável no lugar do placeholder
+							// e incrementa o contador de uso do respectivo valor.
+							let selectedField = $("#values-container input[type='radio']:checked");
+
+			                $.ajax({
+			                    url: selectedField.data('url'),
+			                    data: {selectedField.data('id')},
+			                    success: function(data) {
+									$('#values-container').append(data.htmlMessage);
+			                    }
+			                });
+							if(value)
+								CKEDITOR.instances.workflow.insertHtml(selectedField.val);
+						},
+					}
+				],
+			}
+		],
+	  buttons: [
+	    CKEDITOR.dialog.okButton,
+	    CKEDITOR.dialog.cancelButton
+	 ]
+ 	};
+} );
