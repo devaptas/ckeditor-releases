221d75968ef0bec7bb91ffd42d6345291d7e1f3b
devaptas <36535740+devaptas@users.noreply.github.com>
2019-03-02 11:25:41 -0300

    Melhoria nas variaveis

--- plugins/placeholder/plugin.js
+++ plugins/placeholder/plugin.js
@@ -1,4 +1,4 @@
-﻿
+
 
 'use strict';
 
@@ -18,7 +18,7 @@
 			var lang = editor.lang.placeholder;
 
 			// Registra uma instância do dialog com uma instância do placeholder
-			CKEDITOR.dialog.add( 'placeholder', this.path + 'dialogs/placeholder.js' );
+			CKEDITOR.dialog.add( 'placeholder', this.path + 'dialogs/placeholder.js' ); 
 
 	        editor.addContentsCss( this.path + 'styles/dialogStyle.css' );
 
@@ -35,25 +35,40 @@
 
 					this.setData( 'name', this.element.getText().slice( 2, -2 ) );
 
-					// Ao pressionar a tecla F9, seleciona
+					// Ao pressionar a tecla F2, seleciona
 					// a próxima variável a partir da posição do cursor.
-					this.on('key', function(ev){
-						if (ev.data.keyCode==120){
-							var editor = CKEDITOR.instances.workflow;
+					// this.on('key', function(ev){
+					// 	if (ev.data.keyCode==113){ 
+					// 		var editor = CKEDITOR.instances.workflow;
 							
-						    var range = editor.getSelection().getRanges()[0];
-						    range.setEndAt(editor.editable(), CKEDITOR.POSITION_AFTER_END);
+					// 	    var range = editor.getSelection().getRanges()[0];
+					// 	    range.setEndAt(editor.editable(), CKEDITOR.POSITION_AFTER_END);
 
-						    var walker = new CKEDITOR.dom.walker(range), node;
+					// 	    var walker = new CKEDITOR.dom.walker(range), node;
 
-						    while ((node = walker.next())) {
-						        if($(node.$).hasClass('cke_placeholder') && !this.element.equals(node)) {						    
-									editor.widgets.getByElement(node).focus();
-									break;
-						        }
-						    } 
-						}
-                	});
+					// 	    while ((node = walker.next())) {
+					// 	        if($(node.$).hasClass('cke_placeholder') && !this.element.equals(node)) {						    
+					// 				editor.widgets.getByElement(node).focus();
+					// 				break;
+					// 	        }
+					// 	    } 
+					// 	}
+     //            	});
+
+     				// this.on('edit', function (ev) {
+     				// 	$('.cke_dialog_background_cover').remove();
+     				// });
+
+					// this.on('focus', function(ev){
+					// 	// let dialog = CKEDITOR.dialog.getCurrent();
+					// 	// if (dialog)
+					// 	// 	dialog.hide(); 
+
+					// 	if($(this.wrapper.$.parentElement).hasClass('variable'))
+					// 		editor.execCommand( 'placeholder' );
+					// 	else 
+					// 		editor.insertHtml('');						
+     //            	});
 				},
 
 				data: function() {
